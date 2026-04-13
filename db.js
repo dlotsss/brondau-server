b7c2da510c274f89059b212950a7b88e62991cfb
Ge5pi <sofikomayer@gmail.com>
2026-04-13 18:43:57 +0500

    false

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 const { Pool, types } = pg;
 dotenv.config();
-const isLocal = process.env.LOCAL_DB === 'true';
+const isLocal = process.env.LOCAL_DB === 'false';
 
 const user = isLocal ? 'brondau_user' : process.env.DB_USER;
 const password = isLocal ? 'brondau_password' : process.env.DB_PASSWORD;
